dd94fa5b699ba9866d4686d71ae19c8e487e523b
Michael Milton <ttmigueltt@gmail.com>
2018-08-25 01:36:34 +1000

    Router store inheritance

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,27 +1,27 @@
-import React, {Component} from 'react';
-import {observer, inject} from 'mobx-react';
-import {Router, Route} from 'react-router-dom';
+import React, {Component} from "react";
+import {observer, inject} from "mobx-react";
+import {Router, Route, Switch} from "react-router-dom";
 
-import Typography from '@material-ui/core/Typography';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Grid from '@material-ui/core/Grid';
+import Typography from "@material-ui/core/Typography";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
 
-import QuestionResult from './QuestionResult';
-import TakeQuiz from './TakeQuiz';
-import CreateQuiz from './CreateQuiz';
+import QuestionResult from "./QuestionResult";
+import TakeQuiz from "./TakeQuiz";
+import CreateQuiz from "./CreateQuiz";
 
-import createBrowserHistory from 'history/createBrowserHistory';
-import {syncHistoryWithStore} from 'mobx-react-router';
+import createBrowserHistory from "history/createBrowserHistory";
+import {syncHistoryWithStore} from "mobx-react-router";
 
+const browserHistory = createBrowserHistory();
 
-@inject('router', 'form', 'quiz')
+@inject("router", "form", "quiz")
 @observer
 export default class Page extends Component {
 
     render() {
         const {router, form, quiz} = this.props;
-        const browserHistory = createBrowserHistory();
         const history = syncHistoryWithStore(browserHistory, router);
         return (
             <Router history={history}>
@@ -40,17 +40,19 @@ export default class Page extends Component {
                     </AppBar>
                     <br/>
                     <Grid
-                        justify={'center'}
-                        alignItems={'center'}
+                        justify={"center"}
+                        alignItems={"center"}
                         container
                     >
                         <Grid item
                               style={{
-                                  maxWidth: '600px'
+                                  maxWidth: "600px"
                               }}
                         >
-                            <Route path="/" component={CreateQuiz}/>
-                            <Route path="/quiz" component={TakeQuiz}/>
+                            <Switch>
+                                <Route path="/quiz" component={TakeQuiz}/>
+                                <Route path="/" component={CreateQuiz}/>
+                            </Switch>
                         </Grid>
                     </Grid>
                 </div>
@@ -59,6 +61,6 @@ export default class Page extends Component {
     }
 
     setQuery(query) {
-        this.props.quiz.startQuiz({query: query, prompt: 'image'});
+        this.props.quiz.startQuiz({query: query, prompt: "image"});
     }
 }
--- src/stores/RouterStore.js
+++ src/stores/RouterStore.js
@@ -1,2 +1,12 @@
 import { RouterStore } from 'mobx-react-router';
-export default new RouterStore();
+import {observable, action, computed} from 'mobx';
+import queryString from 'query-string';
+
+class QuizRouterStore extends RouterStore {
+    @computed
+    get queryParams(){
+        return queryString.parse(this.location.search);
+    }
+}
+
+export default new QuizRouterStore();
