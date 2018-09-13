067f954fca8ffdc3abc8a43db090b9441209e8e1
Michael Milton <ttmigueltt@gmail.com>
2018-09-14 09:12:14 +1000

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
     devServer: {
         historyApiFallback: true,
-        publicPath: '/dist/'
+        publicPath: '/',
     },
     entry: [
         './src/index'
